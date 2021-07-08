becbdd3a47c3254dd0df6be482113858ecd61363
Milot Mirdita <milot@mirdita.de>
2021-07-08 15:02:45 +0200

    fix cstranslate on slurm and use pigz

--- pfam_update.sh
+++ pfam_update.sh
@@ -12,7 +12,7 @@ if [ -e ${pfam_lock_file} ] && kill -0 `cat ${pfam_lock_file}`; then
 fi
 rm -f ${pfam_build_dir}/new_relnotes.txt
 curl -o ${pfam_build_dir}/new_relnotes.txt ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/relnotes.txt
-#cmp --silent ${pfam_build_dir}/new_relnotes.txt ${pfam_build_dir}/old_relnotes.txt > /dev/null && rm -f ${pfam_lock_file} && exit 0
+cmp --silent ${pfam_build_dir}/new_relnotes.txt ${pfam_build_dir}/old_relnotes.txt > /dev/null && rm -f ${pfam_lock_file} && exit 0
 mv -f ${pfam_build_dir}/new_relnotes.txt ${pfam_build_dir}/old_relnotes.txt
 
 rm -f ${pfam_build_dir}/Pfam-A.seed.gz ${pfam_build_dir}/Pfam-A.seed
@@ -23,7 +23,7 @@ JOB_ID=$(sbatch -p hh -t 2-0 -n ${NCORES} -N 1 --parsable -o "${LOG_DIR}/pfam_pr
 HH_JOB_ID=$(sbatch -p hh -t 2-0 -n ${NCORES} -N 1 --parsable -o "${LOG_DIR}/pfam_hhblits.log" -d "afterok:$JOB_ID" --kill-on-invalid-dep=yes ./pfam_hhblits_lock.sh)
 #JOB_ID=$(sbatch -p hh -t 2-0 -n ${NCORES} -N 1 --parsable -o "${LOG_DIR}/pfam_hhblits.log" -d "afterok:$JOB_ID" --array=1-11 ./pfam_hhblits.sh)
 
-CS_JOB_ID=$(sbatch -p hh -t 2-0 -n ${NCORES} -N 1 --parsable -o "${LOG_DIR}/pfam_cstranslate.log" -d "afterok:$HH_JOB_ID" --kill-on-invalid-dep=yes ./pfam_cstranslate.sh)
+CS_JOB_ID=$(sbatch -p hh -t 2-0 -c ${NCORES} -N 1 --parsable -o "${LOG_DIR}/pfam_cstranslate.log" -d "afterok:$HH_JOB_ID" --kill-on-invalid-dep=yes ./pfam_cstranslate.sh)
 HM_JOB_ID=$(sbatch -p hh --exclude=hh003 -t 2-0 -n ${NCORES} -N 1 --parsable -o "${LOG_DIR}/pfam_hhmake.log" -d "afterok:$HH_JOB_ID" --kill-on-invalid-dep=yes ./pfam_hhmake.sh)
 
 sbatch -p hh -t 2-0 -n 1 -N 1 -o "${LOG_DIR}/pfam_finalize.log" -d "afterok:${HM_JOB_ID}:${CS_JOB_ID}" --kill-on-invalid-dep=yes ./pfam_finalize.sh
